099ebf3a70220a29400238950c3ea8d1c7156831
Fl0wyn <feramusflorian@icloud.com>
2024-03-02 20:56:46 +0100

    fix typo

--- components/site-hero.tsx
+++ components/site-hero.tsx
@@ -2,13 +2,13 @@ import { siteConfig } from "@/config/site";
 
 export function SiteHero() {
   return (
-    <div className="flex flex-col items-center justify-center h-[18rem] relative z-20">
+    <div className="flex flex-col items-center justify-center h-[22rem] relative z-20">
       <p className="text-neutral-600 dark:text-neutral-200 text-xs sm:text-base">
         {siteConfig.description}
       </p>
 
       <h1 className="text-3xl sm:text-5xl font-bold mt-2">
-        Awersome list{" "}
+        Awesome list{" "}
         <span className="text-amber-500 dark:text-amber-500">creativity</span>
       </h1>
     </div>
